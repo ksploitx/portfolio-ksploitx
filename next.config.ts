86d147d5ab608f01a2e465ee4895fbc49faced2a
Khushneet Singh <singhkhushneet601@gmail.com>
2026-04-30 15:33:14 +0530

    fix: config and package.json updated for next build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: 'export',
   allowedDevOrigins: ['192.168.1.11'],
   images: {
     remotePatterns: [
